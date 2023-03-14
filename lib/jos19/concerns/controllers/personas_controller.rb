8d1d060eec7a74a92d15de8059388c487cb8908b
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-03-14 09:00:06 -0500

    Lista y resumen de persona con listado de actividades donde la persona es asistente y listado de casos donde la persona es victima

--- lib/jos19/concerns/controllers/personas_controller.rb
+++ lib/jos19/concerns/controllers/personas_controller.rb
@@ -12,10 +12,31 @@ module Jos19
         included do
           include Sivel2Gen::Concerns::Controllers::PersonasController
           include Cor1440Gen::Concerns::Controllers::PersonasController
+
+          def atributos_show_jos19
+            atributos_show_cor1440_gen + [
+              :caso_ids,
+            ]
+          end
+
+          def atributos_show
+            atributos_show_jos19
+          end
+
+          def atributos_form_jos19
+            atributos_form_cor1440_gen - [:caso_ids, :familiar_ids]
+          end
+
+          def atributos_form
+            atributos_form_jos19
+          end
+
+
         end  # included
 
         class_methods do
         end
+
       end
     end
   end
