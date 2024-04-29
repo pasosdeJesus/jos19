ab0f84c009bc16a299230e278f2d2e5711e46b3f
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2024-04-29 13:46:33 -0500

    Infraestructura de pruebas

--- bin/regresion.sh
+++ bin/regresion.sh
@@ -53,27 +53,37 @@ if (test "$SALTAUNITARIAS" != "1") then {
   rm -rf cobertura-unitarias/{*,.*}
   if (test -d test/models) then {
     RUTA_RELATIVA=/ ${RAILS} test test/models
-  } fi;
-  if (test -d test/controllers) then {
-    CONFIG_HOSTS=www.example.com RUTA_RELATIVA=/ ${RAILS} test test/controllers
-  } fi;
-  if (test -d test/helpers) then {
-    CONFIG_HOSTS=www.example.com RUTA_RELATIVA=/ ${RAILS} test test/helpers
-  } fi;
-  if (test "$?" != "0") then {
-    echo "No pasaron pruebas de regresión unitarias";
-    exit 1;
-  } fi;
-
-
-  if (test -d test/integration -a "$SALTAINTEGRACION" != "1") then {
-    CONFIG_HOSTS=www.example.com RUTA_RELATIVA=/ bin/rails test `find test/integration -name "*rb" -type f`
     if (test "$?" != "0") then {
-      echo "No pasaron pruebas de integración";
+      echo "No pasaron pruebas de regresión unitarias a modelos";
       exit 1;
     } fi;
   } fi;
+  if (test -d test/controllers) then {
+    CONFIG_HOSTS=www.example.com RUTA_RELATIVA=/ ${RAILS} test test/controllers
+    if (test "$?" != "0") then {
+      echo "No pasaron pruebas de regresión unitarias a controladores";
+      exit 1;
+    } fi;
+  } fi;
+  if (test -d test/helpers) then {
+    CONFIG_HOSTS=www.example.com RUTA_RELATIVA=/ ${RAILS} test test/helpers
+    if (test "$?" != "0") then {
+      echo "No pasaron pruebas de regresión unitarias a auxiliares";
+      exit 1;
+    } fi;
+  } fi;
+} fi;
 
+if (test -d test/integration -a "$SALTAINTEGRACION" != "1") then {
+  echo "== Pruebas de integración unitarias"
+  for i in `find test/integration -name "*rb" -type f`; do
+    echo $i;
+    CONFIG_HOSTS=www.example.com RUTA_RELATIVA=/ bin/rails test $i
+    if (test "$?" != "0") then {
+      echo "No pasó prueba de integración $i";
+      exit 1;
+    } fi;
+  done;
 } fi;
 
 echo "== PRUEBAS DE REGRESIÓN AL SISTEMA"
@@ -88,9 +98,9 @@ if (test "$CI" = "" -a "$SALTACAPYBARA" != "1" -a -d $rutaap/test/system) then {
   } fi;
 } fi;
 
-if (test -f $rutaap/bin/pruebasjs.sh -a "x$NOPRUEBAJS" != "x1" -a -d $rutaap/test/puppeteer) then {
+if (test -f $rutaap/bin/pruebasjs.sh -a -d $rutaap/test/puppeteer -a "x$NOPRUEBAJS" != "x1") then {
   echo "== Con puppeteer"
-  (cd $rutaap; ${RAILS} msip:stimulus_motores; IPDES=127.0.0.1 bin/pruebasjs.sh)
+  (cd $rutaap; ${RAILS} msip:stimulus_motores; bin/pruebasjs.sh)
   if (test "$?" != "0") then {
     echo "No pasaron pruebas del sistema js";
     exit 1;
@@ -101,15 +111,12 @@ echo "== Unificando resultados de pruebas en directorio clásico coverage"
 mkdir -p coverage/
 rm -rf coverage/{*,.*}
 
-if (test "$rutaap" = "test/dummy/" -a "$RC" != "heb412_gen") then {
-  ${RAILS} app:msip:reporteregresion
-} else {
-  ${RAILS} msip:reporteregresion
-} fi;
+${RAILS} ${MSIP_REPORTEREGRESION}
 r=$?
 if (test "$r" != "0") then {
   exit $r;
 } fi;
+
 
 echo "== Copiando resultados para hacerlos visibles en el web en ruta cobertura"
 # Copiar resultados para hacerlos visibles en web
